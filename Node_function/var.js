18c86332dd06453a8dae4084b25c59bcee601e8e
kimsungkwang <skk405@naver.com>
2022-09-01 14:28:12 +0900

    module, exports

--- Node_function/var.js
+++ Node_function/var.js
@@ -3,7 +3,14 @@
 const odd = "홀수";
 const even = "짝수";
 
-module.exports = {
-  odd,
-  even,
-};
+// 두개 같이 쓸 수 없다.
+exports.odd = odd;
+exports.even = even;
+
+// 한가지  ==> module.exports
+// 두가지 이상 ==> exports
+
+// module.exports = {
+//   odd,
+//   even,
+// };
