3216c48175467d01658839cc4874b9ef1300690a
kimsungkwang <skk405@naver.com>
2022-09-03 15:46:19 +0900

    fs html server

--- http_server/server1.js
+++ http_server/server1.js
@@ -3,6 +3,7 @@ const http = require("http");
 // req = 오청 , res = 응답
 const server = http
   .createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
     res.write("<h1>Hello Node</h1>");
     res.write("<p>Hello server</p>");
     res.end("<p>Hello sung kwang</p>");
---
+++ http_server/server2.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs").promises;
+
+// req = 오청 , res = 응답
+const server = http
+  .createServer(async (req, res) => {
+    try {
+      res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+      // 파일 읽기
+      const data = await fs.readFile("server2.html");
+      // 파일 전송
+      res.end(data);
+    } catch (error) {
+      console.error(err);
+      res.writeHead(200, { "Content-type": "text/plain; charset=utf-8" });
+      res.end(err.message);
+    }
+  })
+  .listen(8080);
+
+server.on("listening", () => {
+  console.log("8080 포트에서 서버 대기 중 ");
+});
+
+server.on("error", (err) => {
+  console.log(err);
+});
