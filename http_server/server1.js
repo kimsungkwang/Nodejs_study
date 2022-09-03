54e799c41f11944034e8ec80b771539f056320fe
kimsungkwang <skk405@naver.com>
2022-09-03 15:12:20 +0900

    서버 구축

---
+++ http_server/server1.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+// req = 오청 , res = 응답
+const server = http
+  .createServer((req, res) => {
+    res.write("<h1>Hello Node</h1>");
+    res.write("<p>Hello server</p>");
+    res.end("<p>Hello sung kwang</p>");
+  })
+  .listen(8080);
+
+server.on("listening", () => {
+  console.log("8080 포트에서 서버 대기 중 ");
+});
+
+server.on("error", (err) => {
+  console.log(err);
+});
